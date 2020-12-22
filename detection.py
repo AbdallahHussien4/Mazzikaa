dfbc81fc475e58661c07af38824cbc830456e327
Omar Salama <osalama710@gmail.com>
2020-12-22 15:23:26 +0200

    Removed manual structuring element generartion as we can use the accurate one from open-cv

--- detection.py
+++ detection.py
@@ -12,16 +12,6 @@ def quarterEighthNoteDetection(img):
     horiElement = np.ones((3 * (dim + sl), 1), dtype=np.uint8)
     if dim % 2 == 0:
         dim -= 1
-    '''element = np.zeros((dim, dim), dtype=np.uint8)
-    mid = int(dim/2)
-    print(mid)
-    s = 0
-    dec = False
-    for i in range(dim):
-        element[i, mid - s : mid + s + 1] = 1
-        if s >= mid:
-            dec = True
-        s -= 1 if dec else -1'''
     element = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (dim, dim))
     opened = binary_opening(neg, selem=element)
     opened2 = binary_opening(neg, selem=horiElement)
