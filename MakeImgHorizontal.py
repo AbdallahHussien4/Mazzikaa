09893a80cfa402315f95390e0593fb720eceb1a6
BelalElhossany <42577760+BelalElhossany@users.noreply.github.com>
2020-12-23 22:59:23 +0200

    Create MakeImgHorizontal.py

---
+++ MakeImgHorizontal.py
@@ -0,0 +1,12 @@
+def Make_IMG_HORIZONTAL(BinarizedImg,percesion=1):
+    NegativeBinarizedImg=255-BinarizedImg
+    DegAngles=np.arange(-90,91,percesion)
+    RadAngles=np.deg2rad(DegAngles)
+    Accumilation, angles, rho=hough_line(NegativeBinarizedImg,RadAngles)
+    MaxAccumilation=np.max(Accumilation)
+    rotation_angle=np.average(np.where(Accumilation==MaxAccumilation)[1]) 
+    if(rotation_angle<=90):
+        rotated_img=rotate(BinarizedImg,rotation_angle,resize=True,mode='constant',cval=1)
+    if(rotation_angle>90):
+        rotated_img=rotate(BinarizedImg,rotation_angle-180,resize=True,mode='constant',cval=1)
+    return rotated_img
