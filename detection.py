8a08b8a542a2b5a62f00f25fabf004cf2ef895c6
Omar Salama <osalama710@gmail.com>
2020-12-30 03:32:28 +0200

    Added TA's public test cases

--- detection.py
+++ detection.py
@@ -1,13 +1,15 @@
 from commonfunctions import np, show_images
-from skimage.morphology import binary_opening, binary_closing, binary_erosion
+from skimage.morphology import binary_opening, binary_closing, binary_erosion, binary_dilation
 from skimage.draw import rectangle
 from skimage.measure import find_contours
-from cv2 import cv2  
+from cv2 import cv2
 from StaffLines import getSLsThickness_Whitespaces
+from scipy.ndimage.morphology import binary_fill_holes
+
 
 def getShortestDistance(curPos, linesPos):
     minDist = np.sqrt(abs(np.square(curPos) - np.square(linesPos[0])))
-    minIndex = 0 
+    minIndex = 0
     for index, i in enumerate(linesPos):
         dist = np.sqrt(abs(np.square(curPos) - np.square(i)))
         if dist < minDist:
@@ -15,10 +17,11 @@ def getShortestDistance(curPos, linesPos):
             minIndex = index
     return minIndex
 
+
 def generateLinesArray(sl, dim, firstLine, lastLine):
 
     linesPos = []
-    initLine = firstLine       
+    initLine = firstLine
     for i in range(10, 0, -1):
         linesPos.append(initLine - (dim / 2 + sl / 2)*i)
     linesPos.append(initLine)
@@ -40,68 +43,91 @@ def generateLinesArray(sl, dim, firstLine, lastLine):
 
 
 def quarterEighthNoteDetection(segmentedSymbol, firstLine, lastLine, structElementDim, sl, dim):
-    
-    segmentedSymbol[segmentedSymbol==255]=1
+
+    segmentedSymbol[segmentedSymbol == 255] = 1
     if structElementDim % 2 == 0:
         structElementDim -= 1
-    
-    element = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (structElementDim, structElementDim))
+
+    element = cv2.getStructuringElement(
+        cv2.MORPH_ELLIPSE, (structElementDim, structElementDim))
 
     opened = binary_opening(1 - segmentedSymbol, selem=element)
     contours = find_contours(opened, 0.8)
     for contour in contours:
-        Ymin = min(contour[:,0])
-        Ymax = max(contour[:,0])
+        Ymin = min(contour[:, 0])
+        Ymax = max(contour[:, 0])
         rr = int((Ymax - Ymin) / 2 + Ymin)
         linesPositions = generateLinesArray(sl, dim, firstLine, lastLine)
         lineIndex = getShortestDistance(rr, linesPositions)
         # print(lineIndex)
 
-def halfNoteDetection(segmentedSymbol, firstLine, lastLine, structElementDim, structElementDimMinMax, sl, dim):
+
+def halfNoteDetection(segmentedSymbol, firstLine, lastLine, structElementDimMinMax, sl, dim):
 
     Min = structElementDimMinMax[0]
     Max = structElementDimMinMax[1]
-    
-    filled = fillHalfNoteHeads(segmentedSymbol, Min)
-    
-    segmentedSymbol[segmentedSymbol==255]=1
-    filled[filled==255]=1
-    
-    if structElementDim % 2 == 0:
-        structElementDim -= 1
-    
-    element = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (structElementDim, structElementDim))
-    
-    opened = np.bitwise_xor(binary_opening(1 - segmentedSymbol, selem=element), binary_opening(1 - filled, selem=element)) 
-    #show_images([opened])
+
+    filled = fillHalfNoteHeads(segmentedSymbol, Max)    # Send Max value of whitespaces
+
+    segmentedSymbol[segmentedSymbol == 255] = 1
+    filled[filled == 255] = 1
+
+    if Min % 2 == 0:
+        Min -= 1
+
+    element = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (Min, Min))
+
+    opened = np.bitwise_xor(binary_opening(
+        1 - segmentedSymbol, selem=element), binary_opening(1 - filled, selem=element))
+
+    # show_images([binary_opening(1 - segmentedSymbol, selem=element), binary_opening(1 - filled, selem=element)])
     contours = find_contours(opened, 0.8)
 
     for contour in contours:
-        Xmin = min(contour[:,1])
-        Xmax = max(contour[:,1])
-        Ymin = min(contour[:,0])
-        Ymax = max(contour[:,0])
+        Xmin = min(contour[:, 1])
+        Xmax = max(contour[:, 1])
+        Ymin = min(contour[:, 0])
+        Ymax = max(contour[:, 0])
 
-        if 3 > abs((Xmax - Xmin) - Max) >= 0 and 3 > abs((Ymax - Ymin) - Max) >= 0:
+        if Max / 2 > (Xmax - Xmin) - Max >= 0 and Max / 2 > (Ymax - Ymin) - Max >= 0:
             rr = int((Ymax - Ymin) / 2 + Ymin)
             linesPositions = generateLinesArray(sl, dim, firstLine, lastLine)
             lineIndex = getShortestDistance(rr, linesPositions)
-            print(lineIndex)
-    
-    show_images([opened])
-    
+            print("half:", lineIndex)
+
+    # show_images([opened])
+
 # TODO move 'generateLinesArray' function call to main
+
 
 def fillHalfNoteHeads(image, structElementDim):
 
     img = image.copy()
-    img[img==255]=1
+    img[image == 255] = 0
+    img[image == 1] = 0
+    img[image == 0] = 1
 
     if structElementDim % 2 == 0:
         structElementDim -= 1
-    
-    element = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (structElementDim, structElementDim))
 
-    closed = binary_closing(1 - img, selem=element)
-    return np.uint8((1 - closed) * 255)
+    element = np.ones((structElementDim * 4, 1))
+    opened = binary_opening(img, element)
     
+    element = cv2.getStructuringElement(
+        cv2.MORPH_ELLIPSE, (structElementDim, structElementDim))
+
+    contours = find_contours(opened, 0.8)
+
+    for contour in contours:
+        Xmin = int(min(contour[:, 1]))
+        Xmax = int(max(contour[:, 1]))
+        Ymin = int(min(contour[:, 0]))
+        Ymax = int(max(contour[:, 0]))
+
+        closed = binary_closing(img[Ymin - structElementDim: Ymax + structElementDim,
+                                    Xmin - structElementDim: Xmax + structElementDim], selem=element)
+        opened[Ymin - structElementDim: Ymax + structElementDim,
+               Xmin - structElementDim: Xmax + structElementDim] = closed
+
+    #show_images([opened])
+    return np.uint8((1 - opened) * 255)
--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from RemoveLines import *
 from detection import quarterEighthNoteDetection, fillHalfNoteHeads
 
 def normalizeImage(img):
-    if(img.max() == 1):
-        return np.uint16(img * 255)
+    if(img.max() <= 1):
+        return np.uint8(img * 255)
     return img
 
 img = io.imread('imgs/m1.png', as_gray=True)
@@ -23,21 +23,17 @@ sls, wss = getSLsThickness_Whitespaces(binary, min_max=True)
 sl = int((sls[0] + sls[1]) / 2)
 ws = int((wss[0] + wss[1]) / 2)
 rotated=Make_IMG_HORIZONTAL(binary,1)
-#show_images([rotated])
-segmented = SegmentWithMorph(binary,staff_thick=staffLinesThicc,white_spce=whitespaceLen)
-removedLineImgs=[]
-Symbols=[]
-structElementDim = getSLsThickness_Whitespaces(binary, min_max=True)[1][1]
+segmented = SegmentWithMorph(binary,staff_thick=sl,white_spce=ws)
 for index, image in enumerate(segmented):
     #removedLineImgs.append(removeLines(image,staffLinesThicc))
-    firstLine, lastLine = get_StartingEnding_StaffLinePosition(image, whitespaceLen)
-    NoLines=removeLines(image,staffLinesThicc)
+    firstLine, lastLine = get_StartingEnding_StaffLinePosition(image, ws)
+    NoLines=removeLines(image,sl)
     # Symbols.append(segmentSymbol(NoLines))
     # show_images(segmentSymbol(NoLines))
-    halfNoteDetection(image, firstLine, lastLine, structElementDim, wss, sl, ws)
     for symbol in segmentSymbol(NoLines):
-        quarterEighthNoteDetection(symbol, firstLine, lastLine, structElementDim, sl, ws)
-    #show_images([image])
+        quarterEighthNoteDetection(symbol, firstLine, lastLine, wss[1], sl, ws)
+        halfNoteDetection(symbol, firstLine, lastLine, wss, sl, ws)
+    #show_images([NoLines])
 
 #show_images(removedLineImgs[count])
 #quarterEighthNoteDetection(binary, staffLinesThicc, whitespaceLen)
