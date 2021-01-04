d06cd8ea081327abc5da36b89f6c109ad09277c1
MohamedSamiMoahmed <mohamed.samy9956@gmail.com>
2021-01-04 15:49:28 +0200

    notes for SIFT detection

--- SIFT.py
+++ SIFT.py
@@ -41,9 +41,16 @@ draw_params = dict(matchColor = (0,255,0),
                    singlePointColor = (255,0,0),
                    flags = 0)
 good_points=[]
+#print(matches[0])
 for m, n in matches:
     if m.distance <= 0.5*n.distance:
         good_points.append((m,n))
+################################
+#to calculate the percentage of similarity:
+#get the minmum of length of kp_1,kp_2
+#get the good points length 
+#divide the length of good points by the legnth of minimum
+################################
 img3 = cv2.drawMatchesKnn(binary_compare,kp_1,binary,kp_2,good_points,None,**draw_params)
 show_images([binary,binary_compare,img3],["org","result","match"])
 print(len(good_points))
