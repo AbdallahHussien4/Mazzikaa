96542edd9b92571e478f73badb3d66a92fe8145b
MohamedSamiMoahmed <mohamed.samy9956@gmail.com>
2021-01-12 23:24:15 +0200

    change some files

--- detection.py
+++ detection.py
@@ -86,88 +86,88 @@ def scannedGenerateLinesArray(sl, dim, seg):
     return combined    
 
 
-def quarterEighthNoteDetection(segmentedSymbol, linesPositions, structElementDimMinMax):
+# def quarterEighthNoteDetection(segmentedSymbol, linesPositions, structElementDimMinMax):
 
-    img = segmentedSymbol.copy()
-    img[segmentedSymbol == 255] = 0
-    img[segmentedSymbol == 1] = 0
-    img[segmentedSymbol == 0] = 1
+#     img = segmentedSymbol.copy()
+#     img[segmentedSymbol == 255] = 0
+#     img[segmentedSymbol == 1] = 0
+#     img[segmentedSymbol == 0] = 1
 
-    Max = structElementDimMinMax[1]
+#     Max = structElementDimMinMax[1]
     
-    Min = Max - 1
+#     Min = Max - 1
 
-    if Min % 2 == 0:
-        Min += 1
+#     if Min % 2 == 0:
+#         Min += 1
 
-    if Max % 2 == 0:
-        Max -= 1
+#     if Max % 2 == 0:
+#         Max -= 1
 
-    element = np.ones((Max * 4, int(Min/2)))
+#     element = np.ones((Max * 4, int(Min/2)))
 
-    opened = binary_opening(img, element)
+#     opened = binary_opening(img, element)
 
-    element = np.ones((Min * 3, 1))
+#     element = np.ones((Min * 3, 1))
 
-    opened = binary_opening(img - opened, element)
+#     opened = binary_opening(img - opened, element)
 
-    minElement = cv2.getStructuringElement(
-        cv2.MORPH_ELLIPSE, (Min, Min))
+#     minElement = cv2.getStructuringElement(
+#         cv2.MORPH_ELLIPSE, (Min, Min))
 
-    maxElement = cv2.getStructuringElement(
-        cv2.MORPH_ELLIPSE, (Max, Max))
+#     maxElement = cv2.getStructuringElement(
+#         cv2.MORPH_ELLIPSE, (Max, Max))
 
-    contours = find_contours(opened, 0.8)
+#     contours = find_contours(opened, 0.8)
 
-    structElementDim = Max + 2
+#     structElementDim = Max + 2
 
-    for contour in contours:
-        Xmin = int(min(contour[:, 1]))
-        Xmax = int(max(contour[:, 1]))
-        Ymin = int(min(contour[:, 0]))
-        Ymax = int(max(contour[:, 0]))
-        opened[Ymin: Ymax + structElementDim,
-               Xmin - structElementDim: Xmax] = binary_dilation(binary_erosion(img[Ymin: Ymax + structElementDim,
-                                                                                   Xmin - structElementDim: Xmax], selem=minElement), selem=maxElement)
-        opened[Ymin - structElementDim: Ymax,
-               Xmin: Xmax + structElementDim] = binary_dilation(binary_erosion(img[Ymin - structElementDim: Ymax,
-                                                                                   Xmin: Xmax + structElementDim], selem=minElement), selem=maxElement)
-    matches = matchNoteHead(1 - opened, Min)
-    for rr in matches:
-        lineIndex = getShortestDistance(rr, 0, linesPositions, 0)
+#     for contour in contours:
+#         Xmin = int(min(contour[:, 1]))
+#         Xmax = int(max(contour[:, 1]))
+#         Ymin = int(min(contour[:, 0]))
+#         Ymax = int(max(contour[:, 0]))
+#         opened[Ymin: Ymax + structElementDim,
+#                Xmin - structElementDim: Xmax] = binary_dilation(binary_erosion(img[Ymin: Ymax + structElementDim,
+#                                                                                    Xmin - structElementDim: Xmax], selem=minElement), selem=maxElement)
+#         opened[Ymin - structElementDim: Ymax,
+#                Xmin: Xmax + structElementDim] = binary_dilation(binary_erosion(img[Ymin - structElementDim: Ymax,
+#                                                                                    Xmin: Xmax + structElementDim], selem=minElement), selem=maxElement)
+#     matches = matchNoteHead(1 - opened, Min)
+#     for rr in matches:
+#         lineIndex = getShortestDistance(rr, 0, linesPositions, 0)
 
 
-def halfNoteDetection(img, linesPositions, structElementDimMinMax):
+# def halfNoteDetection(img, linesPositions, structElementDimMinMax):
 
-    Min = structElementDimMinMax[0]
-    Max = structElementDimMinMax[1]
+#     Min = structElementDimMinMax[0]
+#     Max = structElementDimMinMax[1]
 
-    segmentedSymbol = img.copy()
-    # Send Max value of whitespaces
-    filled = fillHalfNoteHeads(segmentedSymbol, Max)
+#     segmentedSymbol = img.copy()
+#     # Send Max value of whitespaces
+#     filled = fillHalfNoteHeads(segmentedSymbol, Max)
 
-    segmentedSymbol[segmentedSymbol == 255] = 1
-    filled[filled == 255] = 1
+#     segmentedSymbol[segmentedSymbol == 255] = 1
+#     filled[filled == 255] = 1
 
-    if Min % 2 == 0:
-        Min -= 1
+#     if Min % 2 == 0:
+#         Min -= 1
 
-    element = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (Min, Min))
+#     element = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (Min, Min))
 
-    opened = np.bitwise_xor(binary_opening(
-        1 - segmentedSymbol, selem=element), binary_opening(1 - filled, selem=element))
+#     opened = np.bitwise_xor(binary_opening(
+#         1 - segmentedSymbol, selem=element), binary_opening(1 - filled, selem=element))
 
-    contours = find_contours(opened, 0.8)
+#     contours = find_contours(opened, 0.8)
 
-    for contour in contours:
-        Xmin = min(contour[:, 1])
-        Xmax = max(contour[:, 1])
-        Ymin = min(contour[:, 0])
-        Ymax = max(contour[:, 0])
+#     for contour in contours:
+#         Xmin = min(contour[:, 1])
+#         Xmax = max(contour[:, 1])
+#         Ymin = min(contour[:, 0])
+#         Ymax = max(contour[:, 0])
 
-        if Max / 2 > (Xmax - Xmin) - Max >= 0 and Max / 2 > (Ymax - Ymin) - Max >= 0:
-            rr = int((Ymax - Ymin) / 2 + Ymin)
-            lineIndex = getShortestDistance(rr, 0, linesPositions, 0)
+#         if Max / 2 > (Xmax - Xmin) - Max >= 0 and Max / 2 > (Ymax - Ymin) - Max >= 0:
+#             rr = int((Ymax - Ymin) / 2 + Ymin)
+#             lineIndex = getShortestDistance(rr, 0, linesPositions, 0)
 
 
 def fillHalfNoteHeads(image, structElementDim):
